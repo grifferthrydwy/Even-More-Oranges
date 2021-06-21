f9b7e4867801ee898bf6cbb0e0fdb2160263b5bb
SimplyCmd <simplycmd00@gmail.com>
2021-06-20 23:01:08 -0600

    fix server crash

--- src/main/java/io/github/simplycmd/even_more_origins/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/simplycmd/even_more_origins/mixin/MinecraftClientMixin.java
@@ -1,5 +1,7 @@
 package io.github.simplycmd.even_more_origins.mixin;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin { // Mixin credit to https://modrinth.com/mod/monsters-in-the-closet
     @Inject(method = "hasOutline", at = @At("RETURN"), cancellable = true)
