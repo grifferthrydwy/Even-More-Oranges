cdefe9740c58b273252040a575519389cc57bd9b
SimplyCmd <simplycmd00@gmail.com>
2021-07-02 10:38:14 -0600

    fix invisibility glow bug

--- src/main/java/io/github/simplycmd/even_more_origins/mixin/MinecraftClientMixin.java
+++ src/main/java/io/github/simplycmd/even_more_origins/mixin/MinecraftClientMixin.java
@@ -1,5 +1,8 @@
 package io.github.simplycmd.even_more_origins.mixin;
 
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import io.github.simplycmd.even_more_origins.power.InvisibilityPower;
+import io.github.simplycmd.even_more_origins.power.SilentPower;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
@@ -15,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MinecraftClientMixin { // Mixin credit to https://modrinth.com/mod/monsters-in-the-closet
     @Inject(method = "hasOutline", at = @At("RETURN"), cancellable = true)
     private void showOutline(Entity entity, CallbackInfoReturnable<Boolean> info) {
-        if (((MinecraftClient) (Object) this).player.isInvisible() && entity instanceof PlayerEntity) {
+        if (((MinecraftClient) (Object) this).player.isInvisible() && entity instanceof PlayerEntity && PowerHolderComponent.hasPower(((MinecraftClient) (Object)this).player, InvisibilityPower.class)) {
             info.setReturnValue(true);
         }
     }
